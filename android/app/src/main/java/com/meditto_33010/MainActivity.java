c127036e3dbe579174519e8728d7c5aed81c2295
Crowdbotics <team@crowdbotics.com>
2022-01-21 18:59:33 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/meditto_33010/MainActivity.java
@@ -0,0 +1,15 @@
+package com.meditto_33010;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "meditto_33010";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "meditto_33010",
+  url: "https://meditto_33010.botics.co",
+  api: "https://meditto_33010.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
